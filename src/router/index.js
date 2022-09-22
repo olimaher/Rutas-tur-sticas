eec1ae1b528ca9e626c4620994cb49c43d4ec038
Olimpo Macea <olimpo3m@gmail.com>
2022-09-22 11:05:08 -0500

    Se han eliminado las views de los destinos porque cada vista proviene de la información de la data.

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,7 @@ import Home from '@/views/Home.vue'
 
 const routes = [
     { path: '/', name: 'Home', component: Home },
-    { path: '/brazil', name: 'Brazil', component: ()=>import('@/views/Brazil.vue')},
-    { path: '/jamaica', name: 'Jamaica', component: () => import('@/views/Jamaica.vue') },
-    { path: '/panama', name: 'Panama', component: () => import('@/views/Panama.vue') },
-    { path: '/hawaii', name: 'Hawaii', component: () => import('@/views/Hawaii.vue') },
-    { path: '/destination/:id', component: () => import('@/views/DestinationShow.vue')}
+    { path: '/destination/:id/:slug', name:'destination.show', component: () => import('@/views/DestinationShow.vue')}
 ]
 
 const router = createRouter({
