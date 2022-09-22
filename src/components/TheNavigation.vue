bb98fe962a89ef14f73d66479ada817d94791c29
Olimpo Macea <olimpo3m@gmail.com>
2022-09-22 09:55:11 -0500

    Convirtiendo la nav en un componente y exportando al app.vue

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="nav">
+
+    <router-link to="/">Home</router-link> 
+    <router-link to="/brazil">Brasil</router-link>
+    <router-link to="/hawaii">Hawaii</router-link>
+    <router-link to="/jamaica">Jamaica</router-link>
+    <router-link to="/panama">Panamá</router-link>
+    
+  </div>
+</template>
